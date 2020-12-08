ebbd819465a99890dea624edb506c545caa5d7d5
Tim Arioto <timarioto@gmail.com>
2020-12-08 11:35:04 +0100

    better solution

--- problems/08-12-20.ts
+++ problems/08-12-20.ts
@@ -1,12 +1,13 @@
 // Pairs of Songs With Total Durations Divisible by 60
 export const numPairsDivisibleBy60 = (time: number[]): number => {
   let result: number = 0
-  for (let i = 0; i < time.length; i++) {
-    for (let j = i+1; j < time.length; j++) {
-      if ((time[i] + time[j]) % 60 === 0) {
-        result++
-      }
-    }
+  const map: any = {}
+
+  for (const t of time) {
+    let remainder = t % 60
+    result += map[(60 - remainder) % 60] || 0
+    map[remainder] = (map[remainder] || 0) + 1
   }
+
   return result
 };
