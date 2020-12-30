3adb5388f824b18c4577ae3200cd3749829603cd
Tim Arioto <timarioto@gmail.com>
2020-12-30 10:00:02 +0100

    rename file

--- tests/29-12-20.test.ts
+++ tests/29-12-20.test.ts
@@ -1,4 +1,4 @@
-import { pseudoPalindromicPaths, TreeNode } from "../problems/30-12-20";
+import { pseudoPalindromicPaths, TreeNode } from "../problems/29-12-20";
 
 test('pseudoPalindromicPaths of [2,3,1,3,1,null,1] to equal 2', () => {
   const root = new TreeNode(2, new TreeNode(3, new TreeNode(3), new TreeNode(1)), new TreeNode(1, null, new TreeNode(1)))
